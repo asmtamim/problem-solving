b2f13e84a3683c8c5e0e930574e70bf1b3b84a95
Sirajum Munir Tamim <asmtamim19@gmail.com>
2023-02-19 11:33:04 +0600

    Add files via upload

---
+++ codeforces-practice/A-112-PetyaAndStrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Codeforces
+{
+    public class PetyaAndStrings
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            str1 = str1.ToLower();
+            str2 = str2.ToLower();
+
+            if (str1 == str2)
+            {
+                Console.WriteLine("0");
+            }
+            else {
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    if (str1[i] < str2[i])
+                    {
+                        Console.WriteLine("-1");
+                        break;
+                    }
+                    if (str1[i] > str2[i])
+                    {
+                        Console.WriteLine("1");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
